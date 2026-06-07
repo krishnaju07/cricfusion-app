c45a12b65a0179c2fed81c2e48ecb882318da399
krishnaju07 <krishnamahi0703@gmail.com>
2026-06-07 14:24:00 +0530

    feat: update request headers for improved CORS handling

--- api/sl-cdn.js
+++ api/sl-cdn.js
@@ -54,15 +54,15 @@ export default async function handler(req) {
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
         'user-agent':
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36',
+        referer: 'https://www.sonyliv.com/',
+        origin: 'https://www.sonyliv.com',
         'sec-ch-ua': '"Chromium";v="148", "Google Chrome";v="148", "Not/A)Brand";v="99"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'none',
-        'sec-fetch-storage-access': 'active',
+        'sec-fetch-site': 'same-site',
         dnt: '1',
-        priority: 'u=1, i',
       },
     })
   } catch (err) {
